733ad654ae5951e748f557b0011ec8cf82c071c3
Aaron Lun <LTLA@users.noreply.github.com>
2016-05-22 00:36:59 +0100

    Switched to work-relevant quotes.

--- quotegenerator.js
+++ quotegenerator.js
@@ -1,49 +1,21 @@
 // Nicked from http://codepen.io/kkoutoup/pen/zxmGLE
 var quoteSource=[
     {
-        quote:"It seems to work. But who knows?",
+        quote:"Check out <i>scran</i>: computational food for the single-cell mind.",
         name:"Aaron"
     },
     {
-        quote:"Close enough is good enough.",
-        name:"Aaron"
+        quote:"Start from the <i>BASiCS</i> of single-cell RNA-seq data analysis.",
+        name:"Catalina"
     },
     {   
-        quote:"Measure once and cut twice.",
+        quote:"Much ado about nothing? Read our normalization paper on zeros in RNA-seq data.",
         name:"Aaron"
-    },
-    {
-        quote:"It builds character.",
-        name:"Aaron"
-    },
-    {
-        quote:"Ah, aesthetics. My old nemesis.",
-        name:"Aaron"
-    },
-    {
-        quote:"Aaron, someone could make a book out of all the stupid things you say.",
-        name:"Nils"
-    },
-    {
-        quote:"Holidays are for chumps.",
-        name:"Aaron"
-    },
-    {
-        quote:"Of course I know what I'm doing.",
-        name:"Aaron"
-    },
-    {
-        quote:"Sigh.",
-        name:"Aaron"
-    },
-    {
-        quote:"Something something random variables something something.",
-        name:"Catalina"
     },
 ];
 
 var randomquote = Math.floor(Math.random()*(quoteSource.length));
 function printquote(){
-    document.write('<i>"'+quoteSource[randomquote].quote+'"</i> &ndash; <b>'+quoteSource[randomquote].name+'</b>');
+    document.write('"'+quoteSource[randomquote].quote+'" &ndash; <b>'+quoteSource[randomquote].name+'</b>');
 }
 
